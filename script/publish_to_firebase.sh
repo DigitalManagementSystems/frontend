a7fc58095b1bd5d767e59408fc7d430d40154525
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2020-04-13 22:48:42 +1200

    Added measurement firebase ID

--- script/publish_to_firebase.sh
+++ script/publish_to_firebase.sh
@@ -15,8 +15,7 @@ export BASE_IMAGE="$(jq -r '.docker_image_tag' < "./build_details.json")"
 
 docker build \
     --build-arg BASE_IMAGE=$BASE_IMAGE \
-    --build-arg TOKEN=$TOKEN \
-    --build-arg PROJECT_ID=$PROJECT_ID \
+    --build-arg FIREBASE_TOKEN=$FIREBASE_TOKEN \
     --build-arg FIREBASE_API_KEY=$FIREBASE_API_KEY \
     --build-arg FIREBASE_AUTH_DOMAIN=$FIREBASE_AUTH_DOMAIN \
     --build-arg FIREBASE_DATABASE_URL=$FIREBASE_DATABASE_URL \
