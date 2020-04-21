99dacdc15d931f8b79aa0a2cd9e358f26e5a95f7
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2020-04-21 22:45:09 +1200

    Adding authorization token to the graphql requests (#41)

--- src/framework/relay/Environment.js
+++ src/framework/relay/Environment.js
@@ -1,11 +1,15 @@
 import { Environment, Network, RecordSource, Store } from 'relay-runtime';
 
+import { reduxStore } from '../redux';
+
 const fetchQuery = async (operation, variables) => {
+  const state = reduxStore.getState();
   const response = await fetch(window._env_.API_GATEWAY_PUBLIC_URL, {
     method: 'POST',
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
+      Authorization: `Bearer ${state.firebase.auth.stsTokenManager.accessToken}`,
     },
     body: JSON.stringify({
       query: operation.text,
