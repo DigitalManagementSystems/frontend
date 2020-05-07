83fa73f928d5d708c2bb379ef9bbced04f1fc3b0
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2020-05-07 19:25:03 +1200

    Added renderSeelct using redux-form (#68)

--- src/components/common/redux-form/index.js
+++ src/components/common/redux-form/index.js
@@ -1,3 +1,4 @@
-export { default as renderTextField } from './TextField';
-export { default as renderRadioGroup } from './RadioGroup';
 export { default as renderAutocomplete } from './Autocomplete';
+export { default as renderRadioGroup } from './RadioGroup';
+export { default as renderSelect } from './Select';
+export { default as renderTextField } from './TextField';
